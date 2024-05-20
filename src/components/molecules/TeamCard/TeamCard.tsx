3caef76c58549b992e07f3652f1a1affc64f47a6
Wiktor Petryszyn <wiktor.petryszyn@rst.com.pl>
2024-05-20 08:43:34 +0200

    feat: card holo

--- src/components/molecules/TeamCard/TeamCard.tsx
+++ src/components/molecules/TeamCard/TeamCard.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/utils/cn";
 import React, {
   CSSProperties,
   MouseEventHandler,
@@ -5,6 +6,7 @@ import React, {
   useMemo,
   useState,
 } from "react";
+import styles from "./TeamCard.module.css";
 
 interface TeamCardProps {
   img: string;
@@ -15,6 +17,7 @@ export const TeamCard: React.FC<TeamCardProps> = ({ img, name }) => {
   const cardRef = createRef<HTMLDivElement>();
 
   const [rotation, setRotation] = useState({ x: 0, y: 0 });
+  const [hologramStyle, setHologramStyle] = useState<CSSProperties>({});
 
   const onMouseMove: MouseEventHandler<HTMLDivElement> = (e) => {
     const cardDiv = cardRef.current;
@@ -32,6 +35,9 @@ export const TeamCard: React.FC<TeamCardProps> = ({ img, name }) => {
     const rotationY = (y / clientHeight) * 20;
 
     setRotation({ x: rotationX, y: rotationY });
+    setHologramStyle({
+      backgroundPosition: `${-x}px ${-y}px`,
+    });
   };
 
   const onMouseLeave = () => {
@@ -55,13 +61,22 @@ export const TeamCard: React.FC<TeamCardProps> = ({ img, name }) => {
         ref={cardRef}
         style={cardStyles}
         className={
-          "transform-gpu relative aspect-[2/3] max-h-[600px] rounded-2xl border-4 border-orange-500 shadow-neon-orange overflow-hidden"
+          "transform-gpu relative aspect-[2/3] max-h-[600px] rounded-2xl border-4 border-orange-500 hover:shadow-neon-orange transition-shadow ease-in-out overflow-hidden"
         }
       >
         <img
           className={"size-full pointer-events-none"}
           alt={`Zdjęcie ${name}`}
           src={img}
+        />
+
+        {/*todo fix holo gradient*/}
+        <div
+          style={hologramStyle}
+          className={cn(
+            styles.hologram,
+            "absolute bg-center bg-cover size-full opacity-10 z-10",
+          )}
         />
 
         <div
