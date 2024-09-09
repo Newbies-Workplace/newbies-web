5bf35749dfea35c7c7bca4d748e88101b0ac5848
JUtupe <jutuper@gmail.com>
2024-09-09 02:53:52 +0200

    feat: more projects

---
+++ src/utils/projects.tsx
@@ -0,0 +1,34 @@
+import { ProjectCard } from "@/components/molecules/ProjectCard/ProjectCard";
+import React, { ReactNode } from "react";
+
+export interface ProjectMetadata {
+  slug: string;
+  title: string;
+  publishedAt: string;
+  summary: string;
+  tags: string[];
+  image?: string;
+  links?: {
+    url: string;
+    title: string;
+    icon?: string;
+  }[];
+}
+
+export interface Project {
+  data: ProjectMetadata;
+  content: React.ReactElement;
+}
+
+export const getAllProjects = async () => {
+  const context = require.context("@public/content/projects", false, /\.mdx$/);
+  const projects: Project[] = context.keys().map((key) => {
+    const project = context(key);
+    return {
+      data: project.metadata,
+      content: project.default({}),
+    };
+  });
+
+  return projects;
+};
